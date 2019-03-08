c64c61f29af5e8401454391067d9ca28e9669570
WustmannMatthias <m.wustmann@redspher.com>
2019-03-08 17:22:01 +0100

    Work in progress on the CMDB ESM tab

--- js/new_application.js
+++ js/new_application.js
@@ -1,10 +1,23 @@
-const APPLICATION_NAME_ROW = "application_name_input_row";
-const PROJECT_ROW = "project_select_row";
-const ENVIRONMENT_ROW = "environment_select_row";
-const SERVICES_PANEL = "services_panel";
+const APPLICATION_NAME_ROW = "#application_name_input_row";
+const PROJECT_ROW = "#project_select_row";
+const ENVIRONMENT_ROW = "#environment_select_row";
+const SERVICES_PANEL = "#services_panel";
+const SECTION_PANEL = ".section_panel";
+const CACHING_SERVICE_PANEL = "#caching_service_panel";
+const DATABASE_PANEL = "#database_panel";
+const APP_VARIABLES_PANEL = "#app_variables_panel";
+const OTHER_VARIABLES_PANEL = "#other_variables_panel";
 
 
-function json_to_options(json) {
+
+/*************************************************************
+ ******************* PREPARATION FUNCTIONS *******************
+ *************************************************************/
+
+/**
+ * Convert a json list into a <option/> tags
+ */
+function jsonToOptions(json) {
 	options = "";
 	for (let i = 0; i < json.length; i++) {
 		option = json[i];
@@ -13,10 +26,14 @@ function json_to_options(json) {
 	return options;
 }
 
-function prepare_service_panel(json) {
-	tbody = "";
-	for (let i = 0; i < json.length; i++) {
-		service = json[i];
+/**
+ * Prepare the array of the service panel
+ */
+function prepareServicesList(services) {
+	tbody_left 	= "";
+	tbody_right = "";
+	for (let i = 0; i < services.length; i++) {
+		service = services[i];
 		$.ajax({
 			method: 'GET',
 			url: 'http://localhost:5000/api/v1.0/model/app/project/' + project + '/environment/' + environment + '/section/client/service/' + service + '/tags',
@@ -24,18 +41,48 @@ function prepare_service_panel(json) {
 			crossdomain: true,
 			async: false
 		}).done(function(data) {
-			tbody += "<tr><td>" + service + "</td>";
+			tbody = "<tr><td>" + service + "</td>";
 			tbody += "<td><select class='form-control' name='" + service + "'>";
 			tbody += "<option value=''>Not selected</option>";
-			tbody += json_to_options(data);
-			tbody += "</select>";
+			tbody += jsonToOptions(data);
+			tbody += "</select></td>";
 			tbody += "</tr>";
+
+			if (i < services.length / 2) 	tbody_left 	+= tbody;
+			else tbody_right += tbody;
 		});
 	}
-	return tbody;
+	return {'left': tbody_left, 'right': tbody_right};
+}
+
+/**
+ * Prepare a list of variables/checkbox
+ */
+function prepareVariableList(variables) {
+	keysNb = Object.keys(variables).length;
+	counter = 0
+
+	tbody_left 	= "";
+	tbody_right = "";
+	for (var variable in variables) {
+		counter += 1
+		tbody =	"<tr><td>" + variable + "</td>";
+		tbody += "<td><input type='checkbox' class='form-control variable_input' name='" + variable + "' /></td>";
+		tbody += "</tr>";
+		if (counter <= keysNb / 2) tbody_left += tbody;
+		else tbody_right += tbody;
+	}
+	return {'left': tbody_left, 'right': tbody_right};
 }
 
 
+
+
+
+
+/**
+ * SCRIPT BEGIN
+ */
 $(function() {
 	// On page loading : prepare projects list
 	$(window).load(function() {
@@ -46,7 +93,7 @@ $(function() {
 			crossdomain: true, 
 			async: false
 		}).done(function(data) {
-			$('#' + PROJECT_ROW + ' select').html(json_to_options(data));
+			$(PROJECT_ROW + ' select').html(jsonToOptions(data));
 		});
 		
 	})
@@ -55,13 +102,13 @@ $(function() {
 	/**
 	 *	When filling application name
 	 */
-	$('#' + APPLICATION_NAME_ROW + ' input').keypress(function(e) {
+	$(APPLICATION_NAME_ROW + ' input').keypress(function(e) {
 		if (e.which == 13) {
 			if ($(this).val().length < 3) {
 				alert('Application name must have at least 3 characters. ');
 			}
 			else {
-				$('#' + PROJECT_ROW).show();
+				$(PROJECT_ROW).show();
 			}
 		}
 	});
@@ -70,7 +117,8 @@ $(function() {
 	/**
 	 *	When choosing a project
 	 */
-	$('#' + PROJECT_ROW + " select").on('change blur', function() {
+	$(PROJECT_ROW + " select").on('change', function() {
+		$(SECTION_PANEL).hide();
 		val = $(this).val();
 		$.ajax({
 			method: 'GET',
@@ -79,9 +127,8 @@ $(function() {
 			crossdomain: true, 
 			async: false
 		}).done(function(data) {
-			$('.section_panel').hide();
-			$('#' + ENVIRONMENT_ROW + ' select').html(json_to_options(data));
-			$('#' + ENVIRONMENT_ROW).show();
+			$(ENVIRONMENT_ROW + ' select').html(jsonToOptions(data));
+			$(ENVIRONMENT_ROW).show();
 		});
 	});
 
@@ -89,21 +136,84 @@ $(function() {
 	/**
 	 *	When choosing an environment
 	 */
-	$('#' + ENVIRONMENT_ROW + " select").on('change blur', function() {
-		project = $('#' + PROJECT_ROW + " select").val();
+	$(ENVIRONMENT_ROW + " select").on('change', function() {
+		project = $(PROJECT_ROW + " select").val();
 		environment = $(this).val();
 
 		$.ajax({
 			method: 'GET',
 			url: 'http://localhost:5000/api/v1.0/model/app/project/' + project + '/environment/' + environment + '/section/client/services',
 			dataType: 'json',
-			crossdomain: true
+			crossdomain: true,
+			async: false
 		}).done(function(data) {
-			$('#' + SERVICES_PANEL + ' table tbody').html(prepare_service_panel(data));
-			$('#' + SERVICES_PANEL).show();
+			services_rows = prepareServicesList(data);
+			console.log(services_rows);
+			$(SERVICES_PANEL + ' table.table_left tbody').html(services_rows.left);
+			$(SERVICES_PANEL + ' table.table_right tbody').html(services_rows.right);
 		});
+
+		$.ajax({
+			method: 'GET',
+			url: 'http://localhost:5000/api/v1.0/model/app/project/' + project + '/environment/' + environment + '/section/app',
+			dataType: 'json',
+			crossdomain: true,
+			async: false
+		}).done(function(data) {
+			app_rows = prepareVariableList(data.app);
+			$(APP_VARIABLES_PANEL + ' table.table_left tbody').html(app_rows.left);
+			$(APP_VARIABLES_PANEL + ' table.table_right tbody').html(app_rows.right);
+		});
+
+		$.ajax({
+			method: 'GET',
+			url: 'http://localhost:5000/api/v1.0/model/app/project/' + project + '/environment/' + environment + '/section/other',
+			dataType: 'json',
+			crossdomain: true,
+			async: false
+		}).done(function(data) {
+			app_rows = prepareVariableList(data.other);
+			$(OTHER_VARIABLES_PANEL + ' table.table_left tbody').html(app_rows.left);
+			$(OTHER_VARIABLES_PANEL + ' table.table_right tbody').html(app_rows.right);
+		});
+
+
+		$(SECTION_PANEL).show();
 	});
 
+
+
+
+
+
+
+
+	/**
+	 * Propose db names when changing the wished number
+	 */
+	$(DATABASE_PANEL + ' input[name=databases_nb]').on("change", function() {
+		nb = $(this).val();
+		tbody = "";
+		for (let i = 0; i < nb; i++) {
+			tbody += "<tr>";
+			tbody += "<td>Database </td>";
+			tbody += "<td><input class='form-control database_input' type='text' name='db" + i  +"' /></td>";
+			tbody += "</tr>";
+		}
+		$(DATABASE_PANEL + ' .variable_tbody').html(tbody);
+	});
+
+	$(CACHING_SERVICE_PANEL + ' input[name=caching_services_nb]').on("change", function() {
+		nb = $(this).val();
+		tbody = "";
+		for (let i = 0; i < nb; i++) {
+			tbody += "<tr>";
+			tbody += "<td>Caching service </td>";
+			tbody += "<td><input class='form-control caching_service_input' type='text' name='cs" + i  +"' /></td>";
+			tbody += "</tr>";
+		}
+		$(CACHING_SERVICE_PANEL + ' .variable_tbody').html(tbody);
+	});
 
 
 });
