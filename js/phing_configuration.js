8d7fd14f6139edb2fa9ae0ac0126ae9671dfc89c
WustmannMatthias <m.wustmann@redspher.com>
2019-04-05 12:23:33 +0200

    Phing configuration page modified : fully depending on an instance now

--- js/phing_configuration.js
+++ js/phing_configuration.js
@@ -4,22 +4,15 @@ const APPLICATION_ROW = "#application_select_row";
 const INSTANCE_ROW = "#instance_select_row";
 const SERVICES_PANEL = "#services_panel";
 const SECTION_PANEL = ".section_panel";
-const INSTANCE_NAME_PANEL = "#instance_name_panel";
 const CACHING_SERVICE_PANEL = "#caching_service_panel";
 const DATABASE_PANEL = "#database_panel";
 const APP_VARIABLES_PANEL = "#app_variables_panel";
 const OTHER_VARIABLES_PANEL = "#other_variables_panel";
 const RESPONSE_PANEL = "#response_panel";
 const NEW_VARIABLE_BUTTONS = ".new_variable_button";
-const NEW_INSTANCE_VALUE = "new_configuration_instance";
-const SUBMIT_PANEL = ".submit_panel";
-const NEW_INSTANCE_SUBMIT_PANEL = "#new_submit_panel";
-const EXISTING_INSTANCE_SUBMIT_PANEL = "#existing_submit_panel";
-const SUBMIT_BUTTON = '.submit_button';
-const DELETE_BUTTON = "#delete_button";
+const SUBMIT_PANEL = "#submit_panel";
 const DOWNLOAD_BUTTON = "#download_button";
-const EDIT_BUTTON = "#edit_button";
-const CREATE_BUTTON = "#create_button";
+const SAVE_BUTTON = "#save_button";
 
 
 
@@ -57,27 +50,6 @@ function sortObjectKeys(obj) {
 	return output.sort();
 }
 
-/**
- * Just check if project/env/application already exists in cmdb push branch or not
- */
-function authorizeInstance(project, environment, application, instance) {
-	authorized = false;
-	$.ajax({
-		method: 'GET',
-		url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instances',
-		dataType: 'json',
-		crossdomain: true,
-		async: false
-	}).done(function(data) { // list of instances
-		if (!data.includes(instance)) {
-			authorized = true;
-		};
-	}).fail(function() {
-		alert("Couldn't reach the server.")
-	});
-
-	return authorized;
-}
 
 
 /**
@@ -154,7 +126,7 @@ function prepareVariableList(variables, checked, section) {
  *	-> new : prepare the panels to create a new configuration
  * 	-> edit : just edit an existing configuration. The panels have to display that existing configuration
  */
-function loadServicesPanel(project, environment, application, instance, mode) {
+function loadServicesPanel(project, environment, application, instance) {
 	service_list = new Array();
 	used_services = {};
 
@@ -171,27 +143,25 @@ function loadServicesPanel(project, environment, application, instance, mode) {
 		return;
 	});
 
-	if (mode == 'edit') {
-		$.ajax({
-			method: 'GET',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/services',
-			dataType: 'json',
-			crossdomain: true,
-			async: false
-		}).done(function(data) {
-			used_services = data;
-		}).fail(function() {
-			alert('Couldn\'t load client configuration from Push');
-			return;
-		});
-	}
+	$.ajax({
+		method: 'GET',
+		url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/services',
+		dataType: 'json',
+		crossdomain: true,
+		async: false
+	}).done(function(data) {
+		used_services = data;
+	}).fail(function() {
+		alert('Couldn\'t load client configuration from Push');
+		return;
+	});
 
 	services_rows = prepareServicesList(service_list, used_services);
 	$(SERVICES_PANEL + ' table.table_left tbody').html(services_rows.left);
 	$(SERVICES_PANEL + ' table.table_right tbody').html(services_rows.right);
 }
 
-function loadAppPanel(project, environment, application, instance, mode) {
+function loadAppPanel(project, environment, application, instance) {
 	variables = {};
 	used_variables = new Array();
 	$.ajax({
@@ -206,19 +176,18 @@ function loadAppPanel(project, environment, application, instance, mode) {
 		alert('Couldn\'t load app variables from Model');
 	});
 
-	if (mode == 'edit') {
-		$.ajax({
-			method: 'GET',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/section/app',
-			dataType: 'json',
-			crossdomain: true,
-			async: false
-		}).done(function(data) {
-			used_variables = data.app;
-		}).fail(function() {
-			alert('Couldn\'t load app configuration from Push');
-		});
-	}
+	$.ajax({
+		method: 'GET',
+		url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/section/app',
+		dataType: 'json',
+		crossdomain: true,
+		async: false
+	}).done(function(data) {
+		used_variables = data.app;
+	}).fail(function() {
+		alert('Couldn\'t load app configuration from Push');
+	});
+
 	app_rows = prepareVariableList(variables, used_variables, 'app');
 	$(APP_VARIABLES_PANEL + ' table.table_left tbody').html(app_rows.left);
 	$(APP_VARIABLES_PANEL + ' table.table_right tbody').html(app_rows.right);
@@ -239,65 +208,60 @@ function loadOtherPanel(project, environment, application, instance, mode) {
 		alert('Couldn\'t load other variables from Model');
 	});
 
-	if (mode == 'edit') {
-		$.ajax({
-			method: 'GET',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/section/other',
-			dataType: 'json',
-			crossdomain: true,
-			async: false
-		}).done(function(data) {
-			used_variables = data.other;
-		}).fail(function() {
-			alert('Couldn\'t load other configuration from Push');
-		});
-	}
+	$.ajax({
+		method: 'GET',
+		url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/section/other',
+		dataType: 'json',
+		crossdomain: true,
+		async: false
+	}).done(function(data) {
+		used_variables = data.other;
+	}).fail(function() {
+		alert('Couldn\'t load other configuration from Push');
+	});
+
 	other_rows = prepareVariableList(variables, used_variables, 'other');
 	$(OTHER_VARIABLES_PANEL + ' table.table_left tbody').html(other_rows.left);
 	$(OTHER_VARIABLES_PANEL + ' table.table_right tbody').html(other_rows.right);
 }
 
 function loadDatabasePanel(project, environment, application, instance, mode) {
-	if (mode == 'edit') {
-		databaseNames = new Array();
-		$.ajax({
-			method: 'GET',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/databases',
-			dataType: 'json',
-			crossdomain: true,
-			async: false
-		}).done(function(data) {
-			databaseNames = data;
-		}).fail(function() {
-			alert('Couldn\'t other configuration from Push');
-		});
-		for (let i = 0; i < databaseNames.length; i++) {
-			newName = databaseNames[i];
-			dbLine = "<tr data-identifier='" + newName + "'><td class='database_name'>" + newName + "</td><td><button data-identifier='" + newName + "' class='btn btn-danger'>Delete</button></td></tr>";
-			$(DATABASE_PANEL + ' .variable_tbody').append(dbLine);
-		}
+	databaseNames = new Array();
+	$.ajax({
+		method: 'GET',
+		url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/databases',
+		dataType: 'json',
+		crossdomain: true,
+		async: false
+	}).done(function(data) {
+		databaseNames = data;
+	}).fail(function() {
+		alert('Couldn\'t other configuration from Push');
+	});
+	for (let i = 0; i < databaseNames.length; i++) {
+		newName = databaseNames[i];
+		dbLine = "<tr data-identifier='" + newName + "'><td class='database_name'>" + newName + "</td><td><button data-identifier='" + newName + "' class='btn btn-danger'>Delete</button></td></tr>";
+		$(DATABASE_PANEL + ' .variable_tbody').append(dbLine);
 	}
 }
 
 function loadCachingServicePanel(project, environment, application, instance, mode) {
-	if (mode == 'edit') {
-		csNames = new Array();
-		$.ajax({
-			method: 'GET',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/caching_services',
-			dataType: 'json',
-			crossdomain: true,
-			async: false
-		}).done(function(data) {
-			csNames = data;
-		}).fail(function() {
-			alert('Couldn\'t other configuration from Push');
-		});
-		for (let i = 0; i < csNames.length; i++) {
-			newName = csNames[i];
-			csLine = "<tr data-identifier='" + newName + "'><td class='caching_service_name'>" + newName + "</td><td><button data-identifier='" + newName + "' class='btn btn-danger'>Delete</button></td></tr>";
-			$(CACHING_SERVICE_PANEL + ' .variable_tbody').append(csLine);
-		}
+	csNames = new Array();
+	$.ajax({
+		method: 'GET',
+		url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance + '/caching_services',
+		dataType: 'json',
+		crossdomain: true,
+		async: false
+	}).done(function(data) {
+		csNames = data;
+	}).fail(function() {
+		alert('Couldn\'t other configuration from Push');
+	});
+	for (let i = 0; i < csNames.length; i++) {
+		newName = csNames[i];
+		csLine = "<tr data-identifier='" + newName + "'><td class='caching_service_name'>" + newName + "</td><td><button data-identifier='" + newName + "' class='btn btn-danger'>Delete</button></td></tr>";
+		$(CACHING_SERVICE_PANEL + ' .variable_tbody').append(csLine);
 	}
 }
 
@@ -334,7 +298,6 @@ $(function() {
 		$(ENVIRONMENT_ROW).hide();
 		$(APPLICATION_ROW).hide();
 		$(INSTANCE_ROW).hide();
-		$(INSTANCE_NAME_PANEL).hide();
 		$(SECTION_PANEL).hide();
 		$(SUBMIT_PANEL).hide();
 		$(RESPONSE_PANEL).hide();
@@ -364,7 +327,6 @@ $(function() {
 	$(ENVIRONMENT_ROW + " select").on('change', function() {
 		$(APPLICATION_ROW).hide();
 		$(INSTANCE_ROW).hide();
-		$(INSTANCE_NAME_PANEL).hide();
 		$(SECTION_PANEL).hide();
 		$(SUBMIT_PANEL).hide();
 		$(RESPONSE_PANEL).hide();
@@ -393,7 +355,6 @@ $(function() {
 	 */
 	$(APPLICATION_ROW + ' select').on('change', function() {
 		$(INSTANCE_ROW).hide();
-		$(INSTANCE_NAME_PANEL).hide();
 		$(SECTION_PANEL).hide();
 		$(SUBMIT_PANEL).hide();
 		$(RESPONSE_PANEL).hide();
@@ -407,13 +368,12 @@ $(function() {
 		environment	= $(ENVIRONMENT_ROW + " select").val();
 		$.ajax({
 			method: 'GET',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instances',
+			url: 'http://10.8.1.72:5000/api/v1.0/push/project/' + project + '/environment/' + environment + '/application/' + application + '/instances',
 			dataType: 'json',
 			crossdomain: true,
 			async: false
 		}).done(function(data) {
 			$(INSTANCE_ROW + ' select').html("<option value=''></option>");
-			$(INSTANCE_ROW + ' select').append("<option value='" + NEW_INSTANCE_VALUE + "'>New configuration instance...</option>");
 			$(INSTANCE_ROW + ' select').append(jsonToOptions(data));
 			$(INSTANCE_ROW).show();
 		});
@@ -437,48 +397,26 @@ $(function() {
 		environment	= $(ENVIRONMENT_ROW + " select").val();
 		application = $(APPLICATION_ROW + " select").val();
 
-		if (instance == NEW_INSTANCE_VALUE) {
-			loadServicesPanel(project, environment, application, instance, 'new');
-			loadAppPanel(project, environment, application, instance, 'new');
-			loadOtherPanel(project, environment, application, instance, 'new');
-			$(INSTANCE_NAME_PANEL).show();
-			$(SECTION_PANEL).show();
-			$(NEW_INSTANCE_SUBMIT_PANEL).show();
-		}
-		else {
-			loadServicesPanel(project, environment, application, instance, 'edit');
-			loadAppPanel(project, environment, application, instance, 'edit');
-			loadOtherPanel(project, environment, application, instance, 'edit');
-			loadDatabasePanel(project, environment, application, instance, 'edit')
-			loadCachingServicePanel(project, environment, application, instance, 'edit')
-			$(INSTANCE_NAME_PANEL).hide();
-			$(SECTION_PANEL).show();
-			$(EXISTING_INSTANCE_SUBMIT_PANEL).show();
-		}
+		loadServicesPanel(project, environment, application, instance);
+		loadAppPanel(project, environment, application, instance);
+		loadOtherPanel(project, environment, application, instance);
+		loadDatabasePanel(project, environment, application, instance)
+		loadCachingServicePanel(project, environment, application, instance)
+
+		$(SECTION_PANEL).show();
+		$(SUBMIT_PANEL).show();
 	});
 
 
 	/**
 	 *	When submitting
 	 */
-	$(SUBMIT_BUTTON).on("click", function() {
+	$(SAVE_BUTTON).on("click", function() {
 		project = $(PROJECT_ROW + " select").val();
 		environment = $(ENVIRONMENT_ROW + " select").val();
 		application = $(APPLICATION_ROW + " select").val();
-		instanceName = "";
+		instance = $(INSTANCE_ROW + " select").val();
 
-		clicked_button = '#' + $(this).attr('id');
-		if (clicked_button == CREATE_BUTTON) {
-			instanceName = $(INSTANCE_NAME_PANEL + ' input').val();
-			authorized = authorizeInstance(project, environment, application, instanceName);
-			if (!authorized) {
-				alert("Instance " + instanceName + " already exists in database.")
-				return;
-			}
-		}
-		else if (clicked_button == EDIT_BUTTON) {
-			instanceName = $(INSTANCE_ROW + ' select').val();
-		}
 
 		applicationVariables = [];
 		$.each($(APP_VARIABLES_PANEL + ' input[type=checkbox]:checked'), function() {
@@ -510,20 +448,17 @@ $(function() {
 		});
 
 
-		if (clicked_button == EDIT_BUTTON) {
-			$.ajax({
-				method: 'DELETE',
-				url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instanceName,
-				contentType: 'application/json; charset=utf-8',
-				crossdomain: true,
-				async: false
-			}).done(function(response) {
-				console.log(response);
-			});
-		}
+		$.ajax({
+			method: 'DELETE',
+			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance,
+			contentType: 'application/json; charset=utf-8',
+			crossdomain: true,
+			async: false
+		}).done(function(response) {
+			console.log(response);
+		});
 
 		data = JSON.stringify({
-			instance: instanceName,
 			client: services,
 			database: databases,
 			caching_service: cachingServices,
@@ -534,7 +469,7 @@ $(function() {
 
 		$.ajax({
 			method: 'POST',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instances',
+			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instance,
 			data: data,
 			contentType: 'application/json; charset=utf-8',
 			dataType: 'json',
@@ -563,54 +498,15 @@ $(function() {
 
 
 
-	/**
-	 *	Delete button press
-	 */
-	$(DELETE_BUTTON).on("click", function() {
-		project = $(PROJECT_ROW + " select").val();
-		environment = $(ENVIRONMENT_ROW + " select").val();
-		application = $(APPLICATION_ROW + " select").val();
-		instanceName = $(INSTANCE_ROW + " select").val();
-
-		$.ajax({
-			method: 'DELETE',
-			url: 'http://10.8.1.72:5000/api/v1.0/push/app/project/' + project + '/environment/' + environment + '/application/' + application + '/instance/' + instanceName,
-			contentType: 'application/json; charset=utf-8',
-			crossdomain: true,
-			async: false
-		}).done(function(response) {
-			console.log(response);
-			if ('success' in response) {
-				msg = response.success;
-				centerClass = 'text-success';
-			}
-			else if ('error' in response) {
-				msg = response.error;
-				centerClass = 'text-danger';
-			}
-			else {
-				msg = "Return status was neither a success or an  error. Object was displayed in js console.";
-				centerClass = 'text-warning';
-			}
-			html = "<center class='" + centerClass + "'>" + msg + "</center>";
-			$(RESPONSE_PANEL + ' .panel-body').html(html);
-			$(RESPONSE_PANEL).show();
-		});
-	});
-
 
 	$(DOWNLOAD_BUTTON).on("click", function() {
 		project = $(PROJECT_ROW + " select").val();
 		environment = $(ENVIRONMENT_ROW + " select").val();
 		application = $(APPLICATION_ROW + " select").val();
-		instanceName = $(INSTANCE_ROW + " select").val();
+		instance = $(INSTANCE_ROW + " select").val();
 
-		window.location = "http://10.8.1.72:5000/api/v1.0/push/app/project/" + project + "/environment/" + environment + "/application/" + application + "/instance/" + instanceName + "/format/ini";
+		window.location = "http://10.8.1.72:5000/api/v1.0/push/app/project/" + project + "/environment/" + environment + "/application/" + application + "/instance/" + instance + "/format/ini";
 	});
-
-
-
-
 
 
 
@@ -708,6 +604,7 @@ $(function() {
 			else alert(section);
 		});
 	});
+
 
 
 
